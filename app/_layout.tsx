36e9f49d583c5c0d31da39fc07af016e981c0be2
Lautaro Gonzalez <146106966+Lautaro10072002@users.noreply.github.com>
2025-06-29 21:49:33 -0300

    Initialize app on login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,21 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { use, useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/components/useColorScheme';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import axios from 'axios';
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
 } from 'expo-router';
 
-const API_KEY = 'dapps1-2025'; // Your API Key
-const URL_PUBLICA = process.env.EXPO_PUBLIC_BACKEND_URL 
-
 export const unstable_settings = {
-  initialRouteName: '(auth)', // This is a default setting, which will be overridden by Redirect
+  // Ensure that reloading on `/modal` keeps a back button present.
+  initialRouteName: '(auth)',
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -29,11 +25,10 @@ export default function RootLayout() {
   const [loaded, error] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
     RobotoRegular: require('../assets/fonts/Roboto-Regular.ttf'),
-    RobotoBold: require('../assets/fonts/Roboto-Bold.ttf')
-    //...FontAwesome.font,
+    RobotoBold: require('../assets/fonts/Roboto-Bold.ttf'),
+    ...FontAwesome.font,
   });
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
     if (error) throw error;
   }, [error]);
@@ -44,56 +39,6 @@ export default function RootLayout() {
     }
   }, [loaded]);
 
-  const [isAuth, setAuth] = useState(false);
-
-  useEffect(() => {
-    try {
-      //ACA HAGO LA LOGICA DEL ASYNC STORAGE
-      const token = async () => {
-        const storedToken = await AsyncStorage.getItem("username");
-        if (storedToken) {
-          // Aquí puedes usar el token almacenado
-          console.log('Token recuperado:', storedToken);
-          setAuth(true);
-        } else {
-          console.log('No se encontró ningún token almacenado.');
-        }
-      }
-    }
-    catch (error) {
-      console.error('Error loading FontAwesome font:', error);
-    }
-  },[])
-
-  useEffect(() => {
-    const checkServerStatus = async () => {
-      try {
-        const pingEndpoint = `${URL_PUBLICA}/ping`;
-        console.log(`Intentando hacer ping al servidor en: ${pingEndpoint}`);
-
-        const response = await axios.get(pingEndpoint, {
-          headers: {
-            'x-api-key': API_KEY, // Asegúrate de que tu API Key esté configurada correctamente
-          },
-        }
-        );
-
-        console.log('Respuesta del servidor al ping:', response.data);
-
-        // La API de ping devuelve "pong" directamente como texto en la respuesta
-        if (response.data === "pong") {
-          
-          console.log('Conexión con la base de datos establecida (recibido "pong").');
-        } else {
-          console.log('El servidor respondió, pero no con "pong". Respuesta:', response.data);
-        }
-      } catch (error) {
-        console.error('Error al hacer ping al servidor:', error);
-      }
-    };
-    checkServerStatus();
-  }, []);
-
   if (!loaded) {
     return null;
   }
@@ -103,12 +48,20 @@ export default function RootLayout() {
 
 function RootLayoutNav() {
   const colorScheme = useColorScheme();
-  
+
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DefaultTheme : DefaultTheme}>
       <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        <Stack.Screen name="RecipePreviewScreen" options={{ headerShown: false }} />
+        <Stack.Screen name="Step" options={{ headerShown: false }} />
+        <Stack.Screen name="editarperfil" options={{ headerShown: false }} />
+        <Stack.Screen name="settings" options={{ headerShown: false }} />
+        <Stack.Screen name="ayuda" options={{ headerShown: false }} />
+        <Stack.Screen name="cambiarclave" options={{ headerShown: false }} />
       </Stack>
     </ThemeProvider>
   );
